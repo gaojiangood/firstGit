4404fbd7f53d60d22b90afa6c80cdcfeaba1d4a2
gaojiangood <gaojiangood@gmail.com>
2015-09-26 22:52:28 +0900

    init

---
+++ firstGit/src/firstGit/CopyOfTest.java
@@ -0,0 +1,11 @@
+package firstGit;
+
+public class CopyOfTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("my first github");
+
+	}
+
+}
